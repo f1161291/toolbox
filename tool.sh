f3046af2ad3a7a0066c9542f3d665f77c9f7d844
f1161291 <78488261+f1161291@users.noreply.github.com>
2025-11-20 16:29:05 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -551,7 +551,8 @@ menu(){
 	green "f. CasaOS 安装"
  	green "g. YAML下载"
     green "h. 3X-UI"
-				green "i. Pve-Debian"
+	green "i. Pve-Debian"
+	green "j. Kejilion脚本"
 	green "x. 一键换源"
 	green "z. Docker 安装"
     red   "dd. 脚本更新"
@@ -576,7 +577,8 @@ menu(){
         f) wget -qO- https://get.casaos.io | sudo bash ;;
 		g) rm -rf toolbox && git clone https://gh.130401.xyz/https://github.com/f1161291/toolbox  && cd toolbox && chmod +x tool.sh  && bash tool.sh ;;
         h) bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)  ;;
-								i) bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/refs/heads/main/vm/debian-vm.sh)"  ;;
+		i) bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/refs/heads/main/vm/debian-vm.sh)"  ;;
+		j) bash <(curl -sL kejilion.sh)
 		x) bash <(curl -sSL https://linuxmirrors.cn/main.sh);; 
   		z) curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun  ;;
 		0) bash <(curl -fsSL https://gh.130401.xyz/https://raw.githubusercontent.com/Aurora-Admin-Panel/deploy/main/install.sh) ;;
