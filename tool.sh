a28fab204e9d9e6f8755e86c96b8ba44b3b516d5
f1161291 <78488261+f1161291@users.noreply.github.com>
2026-08-07 12:39:04 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -187,7 +187,6 @@ menu(){
    	green "e. Rclone安装"
 	green "f. CasaOS 安装"
  	green "g. YAML下载"
-    green "h. X-UI面板"
 	green "i. Pve-Debian"
 	green "j. Kejilion脚本"
     green "k. warp加速"
@@ -204,12 +203,12 @@ menu(){
 		1) root_user ;;
 		2) open_ports ;;
 		3) bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -d 11 -v 64 -p 123456789 ;;
-		4) wget -N --no-check-certificate https://gh.130401.xyz/https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/hysteria.sh && bash hysteria.sh ;;
+		4) wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/hysteria.sh && bash hysteria.sh ;;
 		5) curl -fsSL https://res.oplist.org/script/v4.sh > install-openlist-v4.sh && sudo bash install-openlist-v4.sh ;;
-		6) bash <(curl -Ls https://gh.130401.xyz/https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install.sh) ;;
+		6) bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install.sh) ;;
 		7) apt update -y && apt upgrade -y && apt install git -y && bash <(curl -fsSL https://raw.githubusercontent.com/slobys/SSL-Renewal/main/acme.sh) ;;
 		8) bash <(wget -qO- --no-check-certificate https://gitlab.com/spiritysdx/Oracle-server-keep-alive-script/-/raw/main/oalive.sh) ;;
-		9) wget -q https://gh.130401.xyz/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/ql.sh -O ql.sh && bash ql.sh ;;
+		9) wget -q https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/ql.sh -O ql.sh && bash ql.sh ;;
 		10) tcp_up ;; 
         a) bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh) ;;
 		b) bash <(curl -fsSL https://raw.githubusercontent.com/byJoey/Actions-bbr-v3/main/install.sh) ;; 
@@ -217,18 +216,17 @@ menu(){
   		d) bash <(curl -sSLf https://ailg.ggbond.org/cd2.sh) ;;
 	    e) curl https://rclone.org/install.sh | sudo bash ;;
         f) wget -qO- https://get.casaos.io | sudo bash ;;
-		g) rm -rf toolbox && git clone https://gh.130401.xyz/https://github.com/f1161291/toolbox  && cd toolbox && chmod +x tool.sh  && bash tool.sh ;;
-        h) bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install.sh)  ;;
+		g) rm -rf toolbox && git clone https://gh-proxy.cn/https://github.com/f1161291/toolbox  && cd toolbox && chmod +x tool.sh  && bash tool.sh ;;
 		i) bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/refs/heads/main/vm/debian-vm.sh)"  ;;
 		j) bash <(curl -sL kejilion.sh) ;;
 		k) wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh ;;
 		l) bash -c "$(curl -sSL https://www.linkease.com/rd/fastpve/)" ;;
         n) bash -c "$(curl -sSL https://resource.fit2cloud.com/1panel/package/v2/quick_start.sh)" ;;
 		m) bash <(curl -Ls https://raw.githubusercontent.com/baoweise-bot/aimili-vpngate/main/install.sh) ;;
-		u) wget -N --no-check-certificate https://gh.130401.xyz/https://raw.githubusercontent.com/f1161291/toolbox/main/tool.sh && chmod +x tool.sh && bash tool.sh ;;
+		u) wget -N --no-check-certificate https://gh-proxy.cn/https://raw.githubusercontent.com/f1161291/toolbox/main/tool.sh && chmod +x tool.sh && bash tool.sh ;;
 		x) bash <(curl -sSL https://linuxmirrors.cn/main.sh) ;; 
   		z) curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun  ;;
-		0) bash <(curl -fsSL https://gh.130401.xyz/https://raw.githubusercontent.com/Aurora-Admin-Panel/deploy/main/install.sh) ;;
+		0) bash <(curl -fsSL https://raw.githubusercontent.com/Aurora-Admin-Panel/deploy/main/install.sh) ;;
 		dd) curl -O https://raw.githubusercontent.com/f1161291/other/refs/heads/main/dd.sh && chmod +x dd.sh && bash dd.sh ;;
        
 	esac
